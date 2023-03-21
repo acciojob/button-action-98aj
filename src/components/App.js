f89f57d9b3f0321e396e10da77a9fcbf2de35a4d
98aj <103376356+98aj@users.noreply.github.com>
2023-03-21 07:28:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+   const [change, setChange] = useState('hide')
+  function viwePara(){
+    setChange('show')
+
+  }
 
   return (
     <div className="App" id="main">
       // Do not alter the main div
+   <p id="para" className={change}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    <button id="click" onClick={viwePara}>Click me</button>
     </div>
   );
 }
